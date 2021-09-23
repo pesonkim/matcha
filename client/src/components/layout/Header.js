3bdd5bdd1241647b7944380bad158403ee891059
kpesonen <pesonkim@gmail.com>
2021-09-23 19:37:17 +0300

    dummy users location fix

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -16,29 +16,29 @@ const Header = () => {
 		<header className="h-16 w-full flex justify-center fixed top-0 left-0 right-0 z-50 ui-shadow" style={blur}>
 			<section className="w-full max-w-screen-lg flex justify-between items-center mx-4">
 				<section className="flex items-center">
-					<Link to='/' className='flex items-center hover:opacity-50'>
+					<Link to='/' className='flex items-center hover:opacity-50 select-none'>
 						<p className="text-2xl">matcha </p>
 						<HeartIcon className=' h-6 w-6' />
 					</Link>
 				</section>
 				{loggedIn
 					? <section className="flex items-center">
-						<Link to='/notif' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50'>
+						<Link to='/notif' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50 select-none'>
 							<BellIcon className=' h-8 w-8' />
 							<p className='hidden sm:block'>notifications</p>
 						</Link>
-						<Link to='/join' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50'>
+						<Link to='/join' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50 select-none'>
 							<ChatIcon className=' h-8 w-8' />
 							<p className='hidden sm:block'>matches</p>
 						</Link>
-						<Link to='/profile' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50'>
+						<Link to='/profile' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50 select-none'>
 							<UserCircleIcon className=' h-8 w-8' />
 							<p className='hidden sm:block'>profile</p>
 						</Link>
-						<Link to='/' className='flex flex-col items-center ml-2 xs:ml-4 hover:opacity-50' onClick={() => loginService.logout()} >
+						<section className='flex flex-col items-center ml-2 xs:ml-4 hover:opacity-50 cursor-pointer select-none' onClick={() => loginService.logout()} >
 							<LogoutIcon className=' h-8 w-8' />
 							<p className='hidden sm:block'>logout</p>
-						</Link>
+						</section>
 					</section>
 					: <section className="flex items-center">
 						<Link to='/signup' className='flex flex-col items-center mx-2 xs:mx-4 hover:opacity-50'>
--- client/src/components/pages/Browse.js
+++ client/src/components/pages/Browse.js
@@ -15,6 +15,7 @@ const BrowsePage = () => {
 			{`total: ${users.length}`}
 			<div className="w-full max-w-screen-lg grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-2 md:gap-3 lg:gap-5 mx-auto my-4 px-2">
 				{users.map(profile => {
+					//infinite scroll magic here
 					return (
 						<Link key={profile.id} to={`/browse/${profile.id}`}>
 							<Preview user={profile} />
